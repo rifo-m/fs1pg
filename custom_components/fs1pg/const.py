63f0d36e58b04510d405551156509d26ca4d6d5a
Cés! <123424050+rifo-m@users.noreply.github.com>
2025-01-05 11:15:33 +0100

    Update const.py

--- custom_components/fs1pg/const.py
+++ custom_components/fs1pg/const.py
@@ -4,7 +4,7 @@ from typing import Final
 
 from homeassistant.components.sensor import SensorStateClass, SensorDeviceClass
 from .model import FS1PGSensorEntityDescription
-from homeassistant.const import POWER_WATT, ENERGY_KILO_WATT_HOUR
+#from homeassistant.const import POWER_WATT, ENERGY_KILO_WATT_HOUR
 
 DOMAIN: Final = "fs1pg"
 CONF_MAC_ADDR: Final = "mac"
@@ -22,14 +22,14 @@ SENSOR_TYPES: Final[tuple[FS1PGSensorEntityDescription, ...]] = (
     FS1PGSensorEntityDescription(
         key=ATTR_CURRENT_POWER_W,
         name="Current Power (W)",
-        native_unit_of_measurement=POWER_WATT,
+        #native_unit_of_measurement=POWER_WATT,
         device_class=SensorDeviceClass.POWER,
         state_class=SensorStateClass.MEASUREMENT,
     ),
     FS1PGSensorEntityDescription(
         key=ATTR_TODAY_ENERGY_KWH,
         name="Daily power (KWh)",
-        native_unit_of_measurement=ENERGY_KILO_WATT_HOUR,
+        #native_unit_of_measurement=ENERGY_KILO_WATT_HOUR,
         device_class=SensorDeviceClass.POWER,
         state_class=SensorStateClass.MEASUREMENT,
     ),
